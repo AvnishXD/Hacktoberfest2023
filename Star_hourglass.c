5d2106709332d39b341fa777091e42135be2a849
Avnish Jaltare <145644393+AvnishXD@users.noreply.github.com>
2023-10-31 22:52:57 +0530

    Create Star_hourglass.c

---
+++ Star_hourglass.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,k;               //k is number of spaces
+    printf("Enter no. of rows ");
+    scanf("%d",&n);
+    for(i=n;i>0;i--)
+    {
+        // for(k=(n-i);k>0;k--)
+        // {
+        //     printf(" ");
+        // }
+        //Negative logic
+        for(k=1;k<=(n-i);k++)          //positive logic
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+            {
+                printf("* ");
+            }
+        
+    printf("\n");
+    }
+    for(i=2;i<=n;i++)
+    {
+        // for(k=(n-i);k>0;k--)
+        // {
+        //     printf(" ");
+        // }
+        //Negative logic
+        for(k=1;k<=(n-i);k++)          //positive logic
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+            {
+                printf("* ");
+            }
+        
+    printf("\n");
+    }
+    return 0;
+}
